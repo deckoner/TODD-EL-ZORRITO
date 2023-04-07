bc6b6ef306289802026ad7517f35aca8e5553edf
MiguelAngel456 <80107173+MiguelAngel456@users.noreply.github.com>
2023-04-07 10:55:44 +0200

    SALTOS FUNCIONANDO
    SALTOS FUNCIONANDO(FALTA FUSIONARLOS VCON EL TILEMAPS)

--- TODD EL ZORRITO/Assets/Scripts/ProtagonistaController.cs
+++ TODD EL ZORRITO/Assets/Scripts/ProtagonistaController.cs
@@ -5,20 +5,68 @@ using UnityEngine;
 public class ProtagonistaController : MonoBehaviour
 {
     public float velocidad;
+    public float fuerzaSalto;
+
+    private bool mirandoDerecha = true;
+
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D boxColaider;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        boxColaider = GetComponent<BoxCollider2D>();
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
         procesarMovimiento();
+        procesarSalto();
     }
-    
+
+ /*   bool estaEnSuelo()
+    {
+        Physics2D.BoxCast(boxColaider.bounds.center, new Vector2(boxColaider.bounds.size.x, boxColaider.size.y), 0f, Vector2.down,0.2f,);
+    }*/
+
+
+    void procesarSalto()
+    {
+        //Logica del salto
+        //Condicion if:
+        /*Si se pulsa la tecla espacio*/ /*y*/ /*Esta en el suelo*/
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+    }
     void procesarMovimiento()
     {
-        //logica de movimiento
+        //logica de movimiento lateral
         float inputMovimiento = Input.GetAxis("Horizontal");
 
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-
         rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y );
+
+        GestionarOrientacion(inputMovimiento);
+    }
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        //Girar al prota cuando cambia de direccion
+        
+        //Condiciones del if:
+        /*Si el personaje mira a la derecha*/ /*y*/ /*quiere moverse a la izquierda*/ /*o*/ /*si el personaje mira a la izquierda*/ /*y*/ /*quiere moverse a la derecha*/
+        
+        if ((mirandoDerecha == true && inputMovimiento < 0) || (mirandoDerecha == false && inputMovimiento > 0))
+        {
+            mirandoDerecha = !mirandoDerecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+
+        }
+
     }
 
 }
